6e2f3e6731ea56a2165dc87d34254d0924fa0ada
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-09-12 16:32:43 +0200

    fix: attempt to fix invalid fixture query for postgres (fix #52)

--- migrations/common/fixtures/1_imprint_content.sql
+++ migrations/common/fixtures/1_imprint_content.sql
@@ -1,7 +1,8 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-insert into key_string_values (`key`, `value`) values ('imprint', 'no content here');
+insert into key_string_values ("key", "value") values ('imprint', 'no content here');
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-delete from key_string_values where `key` = 'imprint';
+SET SQL_MODE=ANSI_QUOTES;
+delete from key_string_values where key = 'imprint';
--- utils/common.go
+++ utils/common.go
@@ -44,7 +44,7 @@ func MakeConnectionString(config *models.Config) string {
 
 func mySqlConnectionString(config *models.Config) string {
 	//location, _ := time.LoadLocation("Local")
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&loc=%s",
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&loc=%s&sql_mode=ANSI_QUOTES",
 		config.DbUser,
 		config.DbPassword,
 		config.DbHost,
