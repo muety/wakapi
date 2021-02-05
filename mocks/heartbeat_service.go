e1906abd38df38b94b2a44275364679666f8e04a
Ferdinand Mütsch <ferdinand@muetsch.io>
2021-02-05 18:52:21 +0100

    fix: tests

--- mocks/heartbeat_service.go
+++ mocks/heartbeat_service.go
@@ -10,9 +10,19 @@ type HeartbeatServiceMock struct {
 	mock.Mock
 }
 
+func (m *HeartbeatServiceMock) Insert(heartbeat *models.Heartbeat) error {
+	args := m.Called(heartbeat)
+	return args.Error(0)
+}
+
 func (m *HeartbeatServiceMock) InsertBatch(heartbeats []*models.Heartbeat) error {
 	args := m.Called(heartbeats)
 	return args.Error(0)
+}
+
+func (m *HeartbeatServiceMock) CountByUser(user *models.User) (int64, error) {
+	args := m.Called(user)
+	return args.Get(0).(int64), args.Error(0)
 }
 
 func (m *HeartbeatServiceMock) GetAllWithin(time time.Time, time2 time.Time, user *models.User) ([]*models.Heartbeat, error) {
