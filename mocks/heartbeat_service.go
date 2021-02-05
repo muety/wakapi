8ba3fdcaadfe316f7975b54717689adb74326283
Ferdinand Mütsch <ferdinand@muetsch.io>
2021-02-06 00:35:35 +0100

    fix: tests

--- mocks/heartbeat_service.go
+++ mocks/heartbeat_service.go
@@ -35,6 +35,11 @@ func (m *HeartbeatServiceMock) GetFirstByUsers() ([]*models.TimeByUser, error) {
 	return args.Get(0).([]*models.TimeByUser), args.Error(1)
 }
 
+func (m *HeartbeatServiceMock) GetLatestByOriginAndUser(s string, user *models.User) (*models.Heartbeat, error) {
+	args := m.Called(s, user)
+	return args.Get(0).(*models.Heartbeat), args.Error(1)
+}
+
 func (m *HeartbeatServiceMock) DeleteBefore(time time.Time) error {
 	args := m.Called(time)
 	return args.Error(0)
