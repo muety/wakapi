4f183ed637e4cf2c5a90e38aa76c35d3eaee66a6
Steven Tang <steven@steventang.net>
2022-01-03 12:02:11 +1100

    fix: exec to replace environment.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$WAKAPI_DB_TYPE" == "sqlite3" ] || [ "$WAKAPI_DB_TYPE" == "" ]; then
-  ./wakapi
+  exec ./wakapi
 else
   echo "Waiting for database to come up"
-  ./wait-for-it.sh   "$WAKAPI_DB_HOST:$WAKAPI_DB_PORT" -s -t 60 -- ./wakapi
+  exec ./wait-for-it.sh   "$WAKAPI_DB_HOST:$WAKAPI_DB_PORT" -s -t 60 -- ./wakapi
 fi
