d4760b2838dd1e271faeccc0adc50a6467884982
Steven Tang <yc@steventang.net>
2022-10-07 22:03:44 +1100

    chore: upgrade tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 module.exports = {
-    purge: {
-        enabled: true,
-        mode: 'all',
-        content: ['./views/*.tpl.html'],
-        safelist: [
-            'newsbox-default',
-            'newsbox-warning',
-            'newsbox-danger',
-            'leaderboard-self',
-            'leaderboard-default',
-            'leaderboard-gold',
-            'leaderboard-silver',
-            'leaderboard-bronze',
-        ]
-    },
+    content: [
+        './views/*.tpl.html',
+    ],
+    safelist: [
+        'newsbox-default',
+        'newsbox-warning',
+        'newsbox-danger',
+        'leaderboard-self',
+        'leaderboard-default',
+        'leaderboard-gold',
+        'leaderboard-silver',
+        'leaderboard-bronze',
+    ]
 }
