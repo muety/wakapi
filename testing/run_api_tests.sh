5242df2b7d17acfca0b6589347b807109f47f55f
Steven Tang <yc@steventang.net>
2022-12-04 18:02:36 +1100

    ci: upgrade testing for SQLite

--- testing/run_api_tests.sh
+++ testing/run_api_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-echo "Compiling."
-CGO_ENABLED=0 go build
+set -e
 
 if ! command -v newman &> /dev/null
 then
@@ -9,25 +7,60 @@ then
     exit 1
 fi
 
-cd "$(dirname "$0")"
+script_path=$(realpath "${BASH_SOURCE[0]}")
+script_dir=$(dirname "$script_path")
 
-echo "Creating database and schema ..."
-sqlite3 wakapi_testing.db < schema.sql
+echo "Compiling."
+(cd "$script_dir/.." || exit 1; CGO_ENABLED=0 go build)
 
-echo "Importing seed data ..."
-sqlite3 wakapi_testing.db < data.sql
+cd "$script_dir" || exit 1
 
+# Download previous release (when upgrade testing)
+INITIAL_RUN_EXE="../wakapi"
+if [[ "$2" == "y" ]]; then
+    if [ ! -f wakapi_linux_amd64.zip ]; then
+        curl https://github.com/muety/wakapi/releases/latest/download/wakapi_linux_amd64.zip -O -L
+    fi
+    unzip -o wakapi_linux_amd64.zip
+    INITIAL_RUN_EXE="./wakapi"
+    echo "Running tests with release version"
+fi
+
+# Initialise test data
+case $1 in
+    sqlite|*)
+    rm -f wakapi_testing.db
+
+    echo "Creating database and schema ..."
+    sqlite3 wakapi_testing.db < schema.sql
+
+    echo "Importing seed data ..."
+    sqlite3 wakapi_testing.db < data.sql
+
+    CONFIG="config.testing.yml"
+    ;;
+esac
+
+wait_for_wakapi () {
+    counter=0
+    echo "Waiting for Wakapi to come up ..."
+    until curl --output /dev/null --silent --get --fail http://localhost:3000/api/health; do
+        if [ "$counter" -ge 5 ]; then
+            echo "Waited for 5s, but Wakapi failed to come up ..."
+            exit 1
+        fi
+
+        printf '.'
+        sleep 1
+        counter=$((counter+1))
+    done
+    printf "\n"
+}
+
+# Run tests
 echo "Running Wakapi testing instance in background ..."
-../wakapi -config config.testing.yml &
+"$INITIAL_RUN_EXE" -config "$CONFIG" &
 pid=$!
-
-echo "Waiting for Wakapi to come up ..."
-until $(curl --output /dev/null --silent --get --fail http://localhost:3000/api/health); do
-    printf '.'
-    sleep 1
-done
-
-echo ""
 
 echo "Running test collection ..."
 newman run "wakapi_api_tests.postman_collection.json"
@@ -36,7 +69,16 @@ exit_code=$?
 echo "Shutting down Wakapi ..."
 kill -TERM $pid
 
-echo "Deleting database ..."
-rm wakapi_testing.db
+# Run upgrade tests
+if [[ "$2" == "y" ]]; then
+    echo "Now running migrations with build"
+    ../wakapi -config "$CONFIG" &
+    pid=$!
 
+    wait_for_wakapi
+    echo "Shutting down Wakapi ..."
+    kill -TERM $pid
+fi
+
+echo "Exiting with status $exit_code"
 exit $exit_code
