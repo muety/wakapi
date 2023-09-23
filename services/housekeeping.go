219a24ca943bb1e804cb5e753c4fb25d81c4f0f9
Ferdinand Mütsch <ferdinand@muetsch.io>
2023-09-23 14:12:40 +0200

    chore: pre-warm project stats cache [skip ci]

--- services/housekeeping.go
+++ services/housekeeping.go
@@ -1,10 +1,12 @@
 package services
 
 import (
+	"github.com/duke-git/lancet/v2/slice"
 	"github.com/emvi/logbuch"
 	"github.com/muety/artifex/v2"
 	"github.com/muety/wakapi/config"
 	"github.com/muety/wakapi/models"
+	"github.com/muety/wakapi/utils"
 	"time"
 )
 
@@ -29,39 +31,8 @@ func NewHousekeepingService(userService IUserService, heartbeatService IHeartbea
 }
 
 func (s *HousekeepingService) Schedule() {
-	if s.config.App.DataRetentionMonths <= 0 {
-		return
-	}
-
-	logbuch.Info("scheduling data cleanup")
-
-	_, err := s.queueDefault.DispatchCron(func() {
-		// fetch all users
-		users, err := s.userSrvc.GetAll()
-		if err != nil {
-			config.Log().Error("failed to get users for data cleanup, %v", err)
-			return
-		}
-
-		// schedule jobs
-		for _, u := range users {
-			// don't clean data for subscribed users or when they otherwise have unlimited data access
-			if u.MinDataAge().IsZero() {
-				continue
-			}
-
-			user := *u
-			s.queueWorkers.Dispatch(func() {
-				if err := s.CleanUserDataBefore(&user, user.MinDataAge()); err != nil {
-					config.Log().Error("failed to clear old user data for '%s'", user.ID)
-				}
-			})
-		}
-	}, s.config.App.DataCleanupTime)
-
-	if err != nil {
-		config.Log().Error("failed to dispatch data cleanup jobs, %v", err)
-	}
+	s.scheduleDataCleanups()
+	s.scheduleProjectStatsCacheWarming()
 }
 
 func (s *HousekeepingService) CleanUserDataBefore(user *models.User, before time.Time) error {
@@ -83,4 +54,98 @@ func (s *HousekeepingService) CleanUserDataBefore(user *models.User, before time
 	}
 
 	return nil
+}
+
+func (s *HousekeepingService) WarmUserProjectStatsCache(user *models.User) error {
+	logbuch.Info("pre-warming project stats cache for '%s'", user.ID)
+	if _, err := s.heartbeatSrvc.GetUserProjectStats(user, time.Time{}, utils.BeginOfToday(time.Local), false); err != nil {
+		logbuch.Error("failed to pre-warm project stats cache for '%s', %v", user.ID, err)
+	}
+	return nil
+}
+
+func (s *HousekeepingService) runWarmProjectStatsCache() {
+	// fetch active users
+	users, err := s.userSrvc.GetActive(false)
+	if err != nil {
+		config.Log().Error("failed to get active users for project stats cache warming, %v\n", err)
+		return
+	}
+
+	// fetch user heartbeat counts
+	userHeartbeatCounts, err := s.heartbeatSrvc.CountByUsers(users)
+	if err != nil {
+		config.Log().Error("failed to count user heartbeats for project stats cache warming, %v\n", err)
+		return
+	}
+
+	// schedule jobs
+	for _, c := range userHeartbeatCounts {
+		// only warm cache for users with >= 100k heartbeats (where calculation is expected to take unbearably long)
+		if c.Count < 100_000 {
+			continue
+		}
+
+		user, _ := slice.FindBy[*models.User](users, func(i int, u *models.User) bool {
+			return u.ID == c.User
+		})
+		s.queueWorkers.Dispatch(func() {
+			if err := s.WarmUserProjectStatsCache(user); err != nil {
+				config.Log().Error("failed to pre-warm project stats cache for '%s'", user.ID)
+			}
+		})
+	}
+}
+
+func (s *HousekeepingService) runCleanData() {
+	// fetch all users
+	users, err := s.userSrvc.GetAll()
+	if err != nil {
+		config.Log().Error("failed to get users for data cleanup, %v", err)
+		return
+	}
+
+	// schedule jobs
+	for _, u := range users {
+		// don't clean data for subscribed users or when they otherwise have unlimited data access
+		if u.MinDataAge().IsZero() {
+			continue
+		}
+
+		user := *u
+		s.queueWorkers.Dispatch(func() {
+			if err := s.CleanUserDataBefore(&user, user.MinDataAge()); err != nil {
+				config.Log().Error("failed to clear old user data for '%s'", user.ID)
+			}
+		})
+	}
+}
+
+// individual scheduling functions
+
+func (s *HousekeepingService) scheduleDataCleanups() {
+	if s.config.App.DataRetentionMonths <= 0 {
+		return
+	}
+
+	logbuch.Info("scheduling data cleanup")
+
+	_, err := s.queueDefault.DispatchCron(s.runCleanData, s.config.App.DataCleanupTime)
+	if err != nil {
+		config.Log().Error("failed to dispatch data cleanup jobs, %v", err)
+	}
+}
+
+func (s *HousekeepingService) scheduleProjectStatsCacheWarming() {
+	logbuch.Info("scheduling project stats cache pre-warming")
+
+	_, err := s.queueDefault.DispatchEvery(s.runWarmProjectStatsCache, 24*time.Hour)
+	if err != nil {
+		config.Log().Error("failed to dispatch pre-warming project stats cache, %v", err)
+	}
+
+	// run once initially, 1 min after start
+	if err := s.queueDefault.DispatchIn(s.runWarmProjectStatsCache, 1*time.Minute); err != nil {
+		config.Log().Error("failed to dispatch pre-warming project stats cache, %v", err)
+	}
 }
