99f2b3ced5161c7346acac02c9f3a1362a387eb3
Ferdinand Mütsch <ferdinand@muetsch.io>
2023-12-02 14:02:02 +0100

    feat: implement language filtering in leaders compat endpoint

--- routes/compat/wakatime/v1/leaders.go
+++ routes/compat/wakatime/v1/leaders.go
@@ -9,6 +9,7 @@ import (
 	"github.com/muety/wakapi/utils"
 	"math"
 	"net/http"
+	"strings"
 	"time"
 
 	conf "github.com/muety/wakapi/config"
@@ -47,18 +48,42 @@ func (h *LeadersHandler) RegisterRoutes(router chi.Router) {
 // @Router /compat/wakatime/v1/leaders [get]
 func (h *LeadersHandler) Get(w http.ResponseWriter, r *http.Request) {
 	user := middlewares.GetPrincipal(r)
-	// TODO: language filter param
+	languageParam := strings.ToLower(r.URL.Query().Get("language"))
 	pageParams := utils.ParsePageParamsWithDefault(r, 1, 100)
 	by := models.SummaryLanguage
 
-	globalLeaderboard, err := h.leaderboardSrvc.GetByInterval(h.leaderboardSrvc.GetDefaultScope(), pageParams, true)
+	loadPrimaryLeaderboard := func() (models.Leaderboard, error) {
+		if languageParam == "" {
+			return h.leaderboardSrvc.GetByInterval(h.leaderboardSrvc.GetDefaultScope(), pageParams, true)
+		} else {
+			l, err := h.leaderboardSrvc.GetAggregatedByInterval(h.leaderboardSrvc.GetDefaultScope(), &by, pageParams, true)
+			if err == nil {
+				return l.TopByKey(by, languageParam), err
+			}
+			return nil, err
+		}
+	}
+
+	loadPrimaryUserLeaderboard := func() (models.Leaderboard, error) {
+		if languageParam == "" {
+			return h.leaderboardSrvc.GetByIntervalAndUser(h.leaderboardSrvc.GetDefaultScope(), user.ID, true)
+		} else {
+			l, err := h.leaderboardSrvc.GetAggregatedByIntervalAndUser(h.leaderboardSrvc.GetDefaultScope(), user.ID, &by, true)
+			if err == nil {
+				return l.TopByKey(by, languageParam), err
+			}
+			return nil, err
+		}
+	}
+
+	primaryLeaderboard, err := loadPrimaryLeaderboard()
 	if err != nil {
 		conf.Log().Request(r).Error("error while fetching general leaderboard items - %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("something went wrong"))
 		return
 	}
-	globalLeaderboard.FilterEmpty()
+	primaryLeaderboard.FilterEmpty()
 
 	languageLeaderboard, err := h.leaderboardSrvc.GetAggregatedByInterval(h.leaderboardSrvc.GetDefaultScope(), &by, &utils.PageParams{Page: 1, PageSize: math.MaxUint16}, true)
 	if err != nil {
@@ -69,16 +94,17 @@ func (h *LeadersHandler) Get(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// regardless of page, always show own rank
-	if user != nil && !globalLeaderboard.HasUser(user.ID) {
-		if l, err := h.leaderboardSrvc.GetByIntervalAndUser(h.leaderboardSrvc.GetDefaultScope(), user.ID, true); err == nil {
-			globalLeaderboard.AddMany(l)
+	if user != nil && !primaryLeaderboard.HasUser(user.ID) {
+		if l, err := loadPrimaryUserLeaderboard(); err == nil {
+			primaryLeaderboard.AddMany(l)
 		} else {
 			conf.Log().Request(r).Error("error while fetching own general user leaderboard - %v", err)
 		}
 		// no need to fetch language-leaderboard for user, because not using pagination above
 	}
 
-	vm := h.buildViewModel(globalLeaderboard, languageLeaderboard, user, h.leaderboardSrvc.GetDefaultScope(), pageParams)
+	vm := h.buildViewModel(primaryLeaderboard, languageLeaderboard, user, h.leaderboardSrvc.GetDefaultScope(), pageParams)
+	vm.Language = languageParam
 	helpers.RespondJSON(w, r, http.StatusOK, vm)
 }
 
