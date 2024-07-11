7a508268dd4d6d9838d731bc9bd25c2d1f48f63d
Steven Tang <yc@steventang.net>
2024-07-11 19:42:19 +1000

    ci: run mail tests in workflow
    Resolves #658

--- testing/run_mail_tests.sh
+++ testing/run_mail_tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+set -o nounset -o errexit -o pipefail
+
+cleanup() {
+    echo "Stopping and removing smtpdev ..."
+    docker stop smtp4dev_wakapi &> /dev/null || true
+    docker rm -f smtp4dev_wakapi &> /dev/null
+}
+trap cleanup EXIT
+
+cleanup
 
 echo "Starting smtp4dev in Docker ..."
 docker run -d --rm -p 2525:25 -p 8080:80 --name smtp4dev_wakapi rnwood/smtp4dev
 
 echo "Running tests ..."
-go test -run TestSmtpTestSuite ../services/mail
-
-echo "Killing container again ..."
-docker stop smtp4dev_wakapi
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+go test -count=1 -run TestSmtpTestSuite "$script_dir/../services/mail"
