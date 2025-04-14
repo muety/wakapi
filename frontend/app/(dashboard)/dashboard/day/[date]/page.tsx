2112b3457346884dbbaa77c4fccfcf150e9b1fa9
Jemilu Mohammed <jemiluv8@gmail.com>
2025-04-14 21:21:27 +0000

    update day dashboard config

---
+++ frontend/app/(dashboard)/dashboard/day/[date]/page.tsx
@@ -0,0 +1,24 @@
+import TimeTrackingVisualization from "@/components/day-dashboard/day";
+
+import { DurationData } from "@/lib/types";
+import { fetchData } from "@/actions/data-fetching";
+
+interface iProps {
+  params: { date: string };
+}
+
+export default async function DayPage({ params: { date } }: iProps) {
+  console.log("date");
+  const url = `/v1/users/current/summaries?${new URLSearchParams({
+    date,
+  })}`;
+  const durationData = await fetchData<DurationData>(url);
+  if (!durationData) {
+    return (
+      <h3 className="text-center text-red-500">
+        Error fetching dashboard stats
+      </h3>
+    );
+  }
+  return <TimeTrackingVisualization width={1224} data={durationData} />;
+}
