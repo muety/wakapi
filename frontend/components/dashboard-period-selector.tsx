4a4600d5fe5414d8e92cef41c81cdfadbb5e5b34
Jemilu Mohammed <jemiluv8@gmail.com>
2025-04-16 04:35:43 +0000

    add shortcut to today screen

--- frontend/components/dashboard-period-selector.tsx
+++ frontend/components/dashboard-period-selector.tsx
@@ -2,8 +2,11 @@
 
 import { format, subDays } from "date-fns";
 import { startCase } from "lodash";
-import { Calendar, CalendarArrowDownIcon } from "lucide-react";
+import { CalendarIcon, Zap } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { DateRange } from "react-day-picker";
 
 import {
   DropdownMenu,
@@ -24,6 +27,10 @@ import {
   DashboardRangeQuery,
   getSelectedPeriodLabel,
 } from "@/lib/utils";
+
+import { Button } from "./ui/button";
+import { Calendar } from "./ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export function DashboardPeriodSelector({
   searchParams,
@@ -86,8 +93,22 @@ export function DashboardPeriodSelectorV2({
   searchParams: Record<string, any>;
   baseUrl?: string;
 }) {
+  const router = useRouter();
   const linkToToday = `${baseUrl}/day/${format(new Date(), "yyyy-MM-dd")}`;
   const linkToYesterday = `${baseUrl}/day/${format(subDays(new Date(), 1), "yyyy-MM-dd")}`;
+  const [dateRange, setDateRange] = React.useState<DateRange | undefined>(
+    undefined
+  );
+
+  const applyFilter = React.useCallback(() => {
+    if (dateRange && dateRange.to && dateRange.from) {
+      const start = format(dateRange.from, "yyyy-MM-dd");
+      const end = format(dateRange.to, "yyyy-MM-dd");
+      const route = `${baseUrl}?start=${start}&end=${end}`;
+      router.push(route);
+    }
+  }, [dateRange, router, baseUrl]);
+
   return (
     <div className="flex items-center gap-3">
       <Link
@@ -96,36 +117,31 @@ export function DashboardPeriodSelectorV2({
           "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors",
           "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
           "text-white hover:bg-gray-800 h-9 px-3",
-          "border border-gray-700 hover:border-gray-600"
+          "border hover:border-gray-600"
         )}
       >
-        <Calendar className="w-4 h-4 mr-2" />
+        <Zap className="w-4 h-4 mr-2" />
         Today
       </Link>
-      <Link
-        href={linkToYesterday}
-        className={cn(
-          "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors",
-          "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-          "text-gray-400 hover:bg-gray-800 h-9 px-3",
-          "border border-gray-700 hover:border-gray-600"
-        )}
-      >
-        <CalendarArrowDownIcon className="w-4 h-4 mr-2" />
-        Yesterday
-      </Link>
-      <div className="flex items-center bg-gray-800 rounded-md border border-gray-700 cursor-pointer">
+      <div className="flex items-center hover:bg-gray-800 rounded-md border border-gray-700 cursor-pointer">
         <Select
           onValueChange={(value) => {
             window.location.href = value;
           }}
         >
-          <SelectTrigger className="w-[180px] bg-transparent border-0 text-white focus:ring-0 focus:ring-offset-0">
+          <SelectTrigger
+            className={cn(
+              "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors",
+              "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+              "text-white hover:bg-gray-800 h-9 px-3",
+              "border border-gray-700 hover:border-gray-600"
+            )}
+          >
             <SelectValue
               placeholder={getSelectedPeriodLabel(searchParams || {})}
             />
           </SelectTrigger>
-          <SelectContent className="bg-gray-800 text-white border-gray-700 cursor-pointer">
+          <SelectContent className="text-white border-gray-700 cursor-pointer">
             {Object.values(DashboardRangeQuery).map((query, index) => (
               <SelectItem
                 key={index}
@@ -152,6 +168,41 @@ export function DashboardPeriodSelectorV2({
           </SelectContent>
         </Select>
       </div>
+      <Popover modal={true}>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn("pl-3 text-left font-normal")}
+          >
+            <CalendarIcon className="ml-auto size-4 opacity-80" />
+            {dateRange?.from ? (
+              dateRange.to ? (
+                <>
+                  {format(dateRange.from, "LLL dd, y")} -{" "}
+                  {format(dateRange.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(dateRange.from, "LLL dd, y")
+              )
+            ) : null}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            selected={dateRange}
+            onSelect={setDateRange as any}
+            disabled={(date: Date) => date > new Date()}
+            numberOfMonths={2}
+          />
+          <div className="flex justify-end items-center px-2">
+            <Button onClick={applyFilter} className="block w-full" size="sm">
+              Apply
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
