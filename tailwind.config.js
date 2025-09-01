95f465ae307a35eb305cf22a798eccd78b08b2d0
Max Koon <22125083+k2on@users.noreply.github.com>
2025-09-01 12:02:52 -0400

    refactor: color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,20 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     theme: {
+        darkMode: 'class',
         extend: {
             colors: {
                 green: colors.emerald,
+                background: 'var(--background)',
+                card: 'var(--card)',
+
+                muted: 'var(--muted)',
+
+                secondary: 'var(--secondary)',
+                primary: 'var(--primary)',
+                foreground: 'var(--foreground)',
+                accent: 'rgb(var(--accent) / <alpha-value>)',
+                danger: 'rgb(var(--danger) / <alpha-value>)',
             },
             width: {
                 '16': '4rem',
