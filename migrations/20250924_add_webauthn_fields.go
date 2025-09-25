3287e117c18fd669aa9eb5f6007cbed191364ec5
Daniel Hagen <5039466+dbhagen@users.noreply.github.com>
2025-09-25 10:27:45 -0500

    fix: refactor migration for sonarqube findings
    Signed-off-by: Daniel Hagen <5039466+dbhagen@users.noreply.github.com>

--- migrations/20250924_add_webauthn_fields.go
+++ migrations/20250924_add_webauthn_fields.go
@@ -1,45 +1,49 @@
 package migrations
 
 import (
-	"log/slog"
-
 	"github.com/muety/wakapi/config"
 	"github.com/muety/wakapi/models"
 	"gorm.io/gorm"
 )
 
 func init() {
+	const name = "20250924-add_webauthn_fields"
 	f := migrationFunc{
-		name: "20250924-add_webauthn_fields",
-		f:    migration20250924_add_webauthn_fields,
+		name: name,
+		f: func(db *gorm.DB, cfg *config.Config) error {
+			if hasRun(name, db) {
+				return nil
+			}
+			migrator := db.Migrator()
+
+			if err := db.Transaction(func(tx *gorm.DB) error {
+				if migrator.HasTable(&models.User{}) {
+					if !migrator.HasColumn(&models.User{}, "webauthn_credentials") {
+						if err := migrator.AddColumn(&models.User{}, "webauthn_credentials"); err != nil {
+							return err
+						}
+					}
+
+					if !migrator.HasColumn(&models.User{}, "webauthn_session") {
+						if err := migrator.AddColumn(&models.User{}, "webauthn_session"); err != nil {
+							return err
+						}
+					}
+
+					if !migrator.HasColumn(&models.User{}, "webauthn_session_expiry") {
+						if err := migrator.AddColumn(&models.User{}, "webauthn_session_expiry"); err != nil {
+							return err
+						}
+					}
+				}
+				return nil
+			}); err != nil {
+				return err
+			}
+
+			setHasRun(name, db)
+			return nil
+		},
 	}
 	registerPreMigration(f)
-}
-
-func migration20250924_add_webauthn_fields(db *gorm.DB, cfg *config.Config) error {
-	migrator := db.Migrator()
-	
-	if migrator.HasTable(&models.User{}) {
-		slog.Info("adding webauthn fields to users table")
-		
-		if !migrator.HasColumn(&models.User{}, "webauthn_credentials") {
-			if err := migrator.AddColumn(&models.User{}, "webauthn_credentials"); err != nil {
-				return err
-			}
-		}
-		
-		if !migrator.HasColumn(&models.User{}, "webauthn_session") {
-			if err := migrator.AddColumn(&models.User{}, "webauthn_session"); err != nil {
-				return err
-			}
-		}
-		
-		if !migrator.HasColumn(&models.User{}, "webauthn_session_expiry") {
-			if err := migrator.AddColumn(&models.User{}, "webauthn_session_expiry"); err != nil {
-				return err
-			}
-		}
-	}
-	
-	return nil
 }
