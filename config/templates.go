b3924c41191cdbad2fefdd22c8cf3308bb59616a
Ferdinand Mütsch <ferdinand@muetsch.io>
2025-11-07 11:08:54 +0100

    docs: setup instructions page

--- config/templates.go
+++ config/templates.go
@@ -4,6 +4,7 @@ const (
 	IndexTemplate         = "index.tpl.html"
 	LoginTemplate         = "login.tpl.html"
 	ImprintTemplate       = "imprint.tpl.html"
+	SetupTemplate         = "setup.tpl.html"
 	SignupTemplate        = "signup.tpl.html"
 	SetPasswordTemplate   = "set-password.tpl.html"
 	ResetPasswordTemplate = "reset-password.tpl.html"
--- main.go
+++ main.go
@@ -240,6 +240,7 @@ func main() {
 	homeHandler := routes.NewHomeHandler(userService, keyValueService)
 	loginHandler := routes.NewLoginHandler(userService, mailService, keyValueService)
 	imprintHandler := routes.NewImprintHandler(keyValueService)
+	setupHandler := routes.NewSetupHandler(userService)
 	leaderboardHandler := condition.Ternary[bool, routes.Handler](config.App.LeaderboardEnabled, routes.NewLeaderboardHandler(userService, leaderboardService), routes.NewNoopHandler())
 	miscHandler := routes.NewMiscHandler(userService)
 
@@ -279,6 +280,7 @@ func main() {
 	homeHandler.RegisterRoutes(rootRouter)
 	loginHandler.RegisterRoutes(rootRouter)
 	imprintHandler.RegisterRoutes(rootRouter)
+	setupHandler.RegisterRoutes(rootRouter)
 	summaryHandler.RegisterRoutes(rootRouter)
 	leaderboardHandler.RegisterRoutes(rootRouter)
 	projectsHandler.RegisterRoutes(rootRouter)
