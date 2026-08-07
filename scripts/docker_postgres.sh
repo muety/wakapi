e1395e4cd866326eb18ff611533f1032b6a4aba7
Ferdinand Mütsch <ferdinand@muetsch.io>
2026-08-07 15:23:38 +0200

    chore: update postgres docker startscript for testing [skip-ci]

--- scripts/docker_postgres.sh
+++ scripts/docker_postgres.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 5432:5432 -e POSTGRES_DB=wakapi_local -e POSTGRES_USER=wakapi_user -e POSTGRES_PASSWORD=wakapi --name wakapi-postgres postgres
+docker volume create wakapi-postgres-data
+docker run -d -p 5432:5432 -e POSTGRES_DB=wakapi_local -e POSTGRES_USER=wakapi_user -e POSTGRES_PASSWORD=wakapi -v wakapi-postgres-data:/var/lib/postgresql --name wakapi-postgres postgres:18-alpine
