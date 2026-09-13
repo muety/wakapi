1ff103b9f930307529c4c2a1b6e63affda46cd60
Ferdinand Mütsch <ferdinand@muetsch.io>
2026-09-13 11:18:41 +0200

    chore: update editor and os colors feat: extend color conversion script to read colors from wakatime website [skip ci]

--- scripts/convert_colors.py
+++ scripts/convert_colors.py
@@ -1,11 +1,50 @@
-# convert colors from the format provided by https://raw.githubusercontent.com/ozh/github-colors/master/colors.json to what wakapi wants
+#!/usr/bin/env python3
 
+"""
+usage: convert_colors.py [-h] [--languages | --no-languages] [--editors | --no-editors]
+                         [--operating_systems | --no-operating_systems]
+
+options:
+  -h, --help            show this help message and exit
+  --languages, --no-languages
+  --editors, --no-editors
+  --operating_systems, --no-operating_systems
+"""
+
+import argparse
 import json
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json') as f:
-        colors = json.load(f)
+from lxml import html as lxml_html  # pip install lxml
 
-    result = {k: v['color'] for k, v in colors.items()}
+
+def fetch(url):
+    with urllib.request.urlopen(url) as f:
+        return f.read().decode('utf-8')
+
+
+def fetch_wakatime_website(section):
+    tree = lxml_html.fromstring(fetch(f'https://wakatime.com/colors/{section}'))
+    return {el.attrib['title']: el.xpath('./div[1]/text()')[0].strip()
+            for el in tree.xpath('//span[@class="editor-icon tip"]')}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    for s in ('languages', 'editors', 'operating_systems'):
+        parser.add_argument(f'--{s}', action=argparse.BooleanOptionalAction, default=True)
+    args = parser.parse_args()
+
+    result = {}
+    if args.languages:
+        result['languages'] = {k: v['color'] for k, v in json.loads(fetch('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json')).items()}
+    if args.editors:
+        result['editors'] = fetch_wakatime_website('editors')
+    if args.operating_systems:
+        result['operating_systems'] = fetch_wakatime_website('operating_systems')
+
     print(json.dumps(result, indent=4))
+
+
+if __name__ == '__main__':
+    main()
